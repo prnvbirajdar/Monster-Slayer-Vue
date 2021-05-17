0ffc0aaa7c63aefe6f06df69b4a15a5b0122bfc7
prnvbirajdar <prnvbirajdar@gmail.com>
2021-05-17 00:41:06 -0400

    seperated monster and players attacks, disabled special attack for rare uses

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
+function getRandomInteger(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
   data() {
     return {
-      monsterHealth: 20,
-      yourHealth: 20,
+      monsterHealth: 100,
+      yourHealth: 100,
+      attackCount: 0,
     };
   },
   methods: {
-    getRandomInteger(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min)) + min;
+    attackMonster() {
+      this.attackCount++;
+      this.monsterHealth -= getRandomInteger(5, 12);
+      this.attackPlayer();
     },
+
+    attackPlayer() {
+      this.yourHealth -= getRandomInteger(8, 15);
+    },
+
+     superAttack() {
+
+        
+      console.log(this.monsterHealth - getRandomInteger(10, 21));
+    },
+
     attack() {
       if (this.monsterHealth > 0 && this.yourHealth > 0) {
-        this.monsterHealth = this.monsterHealth - this.getRandomInteger(3, 11);
-        this.yourHealth = this.yourHealth - this.getRandomInteger(3, 11);
+        this.monsterHealth = this.monsterHealth - getRandomInteger(3, 11);
+        this.yourHealth = this.yourHealth - getRandomInteger(3, 11);
       } else if (this.monsterHealth > 0 && this.yourHealth <= 0) {
         console.log("monster won");
       } else if (this.monsterHealth <= 0 && this.yourHealth > 0) {
@@ -23,11 +41,9 @@ const app = Vue.createApp({
 
       console.log(this.monsterHealth, this.yourHealth);
     },
-    superAttack() {
-      console.log(this.monsterHealth - this.getRandomInteger(10, 21));
-    },
+   
     heal() {
-      console.log(this.yourHealth + this.getRandomInteger(10, 21));
+      console.log(this.yourHealth + getRandomInteger(10, 21));
     },
   },
 });
