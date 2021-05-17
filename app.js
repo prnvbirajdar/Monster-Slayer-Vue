e2f9103c73713277116e492f4cc485426e1c5605
prnvbirajdar <prnvbirajdar@gmail.com>
2021-05-17 00:47:27 -0400

    heal working

--- app.js
+++ app.js
@@ -23,27 +23,16 @@ const app = Vue.createApp({
       this.yourHealth -= getRandomInteger(8, 15);
     },
 
-     superAttack() {
-
-        
-      console.log(this.monsterHealth - getRandomInteger(10, 21));
+    superAttack() {
+      this.attackCount++;
+      this.monsterHealth -= getRandomInteger(10, 25);
+      this.attackPlayer();
     },
 
-    attack() {
-      if (this.monsterHealth > 0 && this.yourHealth > 0) {
-        this.monsterHealth = this.monsterHealth - getRandomInteger(3, 11);
-        this.yourHealth = this.yourHealth - getRandomInteger(3, 11);
-      } else if (this.monsterHealth > 0 && this.yourHealth <= 0) {
-        console.log("monster won");
-      } else if (this.monsterHealth <= 0 && this.yourHealth > 0) {
-        console.log("you won");
-      }
-
-      console.log(this.monsterHealth, this.yourHealth);
-    },
-   
     heal() {
-      console.log(this.yourHealth + getRandomInteger(10, 21));
+      this.attackCount++;
+      this.yourHealth += getRandomInteger(8, 15);
+      this.attackPlayer();
     },
   },
 });
